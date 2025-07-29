630626d550c9d87db0d8e5c28a94f826cb1e207c
Ceeed <cedrickagapito@gmail.com>
2025-07-29 12:55:12 +0800

    Modified HeaderStats to only display at tables view

--- src/components/Headers/HeaderStats.js
+++ src/components/Headers/HeaderStats.js
@@ -1,10 +1,20 @@
 import React from "react";
+import { useLocation } from "react-router-dom/cjs/react-router-dom";
 
 // components
 
 import CardStats from "components/Cards/CardStats.js";
 
 export default function HeaderStats() {
+  const location = useLocation();
+
+  if (location.pathname !== "/admin/tables") {
+    return (
+      <>
+        <div className="relative bg-lightBlue-600 md:pt-32 pb-32 pt-12"></div>
+      </>
+    );
+  }
   return (
     <>
       {/* Header */}
