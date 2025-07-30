c226f0c029b19edc790c9c73a99dcc4fcdb44b41
Ceeed <cedrickagapito@gmail.com>
2025-07-30 19:23:02 +0800

    Modified Login Page - removed unused imports

--- src/layouts/Auth.js
+++ src/layouts/Auth.js
@@ -1,10 +1,6 @@
 import React from "react";
 import { Switch, Route, Redirect } from "react-router-dom";
 
-// components
-
-import FooterSmall from "components/Footers/FooterSmall.js";
-
 // views
 
 import Login from "views/auth/Login.js";
