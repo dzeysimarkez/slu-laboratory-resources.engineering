db515b2c0570afc3f858c03760aa5c26c1e5fbfa
Ceeed <cedrickagapito@gmail.com>
2025-08-21 17:46:36 +0800

    test modal

--- src/views/auth/Register.js
+++ src/views/auth/Register.js
@@ -1,9 +1,19 @@
-import React from "react";
-
+import React, {useState} from "react";
+import EditItemModal from "../../components/Modals/EditItemModal";
+// import EditItemForm from "../../components/Forms/EditItemForm";
 export default function Register() {
+    const [isModalOpen, setIsModalOpen] = useState(true);
   return (
     <>
-      <div className="container mx-auto px-4 h-full">
+    {/* <EditItemModal onClose={() => setIsModalOpen(false)}>
+       <EditItemForm></EditItemForm> 
+    </EditItemModal> */}
+          {isModalOpen && (
+            <EditItemModal onClose={() => setIsModalOpen(false)}>
+              {/* <EditItemForm item={selectedItem} onUpdate={handleUpdateItem} /> */}
+            </EditItemModal>
+          )}
+      {/* <div className="container mx-auto px-4 h-full">
         <div className="flex content-center items-center justify-center h-full">
           <div className="w-full lg:w-6/12 px-4">
             <div className="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-blueGray-200 border-0">
@@ -119,7 +129,7 @@ export default function Register() {
             </div>
           </div>
         </div>
-      </div>
+      </div> */}
     </>
   );
 }
