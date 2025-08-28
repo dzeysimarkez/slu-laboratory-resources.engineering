954b7858e87b9a9b09f6b7738253c93d0ca758f7
Ceeed <cedrickagapito@gmail.com>
2025-08-28 15:40:53 +0800

    restyled login page

--- src/views/auth/Login.js
+++ src/views/auth/Login.js
@@ -48,8 +48,8 @@ export default function Login() {
         <div className="flex content-center items-center justify-center h-full">
           <div className="w-full lg:w-4/12 px-4">
             <div className="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-blueGray-200 border-0">
-              {/* <div className="rounded-t mb-0 px-6 py-6">
-                <div className="text-center mb-3">
+              <div className="rounded-t mb-0 px-6 py-6">
+                {/* <div className="text-center mb-3">
                   <h6 className="text-blueGray-500 text-sm font-bold">
                     Sign in with
                   </h6>
@@ -66,9 +66,9 @@ export default function Login() {
                     />
                     Google
                   </button>
-                </div>
+                </div> */}
                 <hr className="mt-6 border-b-1 border-blueGray-300" />
-              </div> */}
+              </div>
               <div className="flex-auto px-4 lg:px-10 py-10 pt-0">
                 <div className="text-blueGray-400 text-center mb-3 font-bold">
                   <small>Sign in with credentials</small>
