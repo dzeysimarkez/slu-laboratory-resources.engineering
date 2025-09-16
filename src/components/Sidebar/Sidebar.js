ccececf9094cf764b8c79df66873d1612dc984b9
Ceeed <cedrickagapito@gmail.com>
2025-09-16 17:32:10 +0800

    added Add User

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -172,6 +172,27 @@ export default function Sidebar() {
                   Add new items
                 </Link>
               </li>
+              <li className="items-center">
+                <Link
+                  className={
+                    "text-xs uppercase py-3 font-bold block " +
+                    (window.location.href.indexOf("/admin/settings") !== -1
+                      ? "text-lightBlue-500 hover:text-lightBlue-600"
+                      : "text-blueGray-700 hover:text-blueGray-500")
+                  }
+                  to="/admin/adduser"
+                >
+                  <i
+                    className={
+                      "fas fa-tools mr-2 text-sm " +
+                      (window.location.href.indexOf("/admin/adduser") !== -1
+                        ? "opacity-75"
+                        : "text-blueGray-300")
+                    }
+                  ></i>{" "}
+                  Add new user
+                </Link>
+              </li>
               
               {/* <li className="items-center">
                 <Link
