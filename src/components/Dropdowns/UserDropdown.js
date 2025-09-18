141d9b1a9c010676065f07ede862168049e4d849
Ceeed <cedrickagapito@gmail.com>
2025-09-18 10:28:34 +0800

    removed extra search

--- src/components/Dropdowns/UserDropdown.js
+++ src/components/Dropdowns/UserDropdown.js
@@ -62,7 +62,7 @@ const UserDropdown = () => {
         >
           Logout
         </a>
-        <a
+        {/* <a
           href="#pablo"
           className={
             "text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-blueGray-700"
@@ -89,7 +89,7 @@ const UserDropdown = () => {
           onClick={(e) => e.preventDefault()}
         >
           Seprated link
-        </a>
+        </a> */}
       </div>
     </>
   );
