f48f9bb953bf1b8e5b64a16c09880cd4e6636421
Ceeed <cedrickagapito@gmail.com>
2025-10-02 17:29:17 +0800

    update icon

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -140,7 +140,7 @@ export default function Sidebar() {
                 >
                   <i
                     className={
-                      "fas fa-solid fa-boxes-stacked mr-2 text-sm " +
+                      "fa-solid fa-boxes-stacked mr-2 text-sm " +
                       (window.location.href.indexOf("/admin/items") !== -1
                         ? "opacity-75"
                         : "text-blueGray-300")
