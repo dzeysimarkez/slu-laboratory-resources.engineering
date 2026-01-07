7a2191c4e3c037e6aea649e7f6a181a23235e27c
Ceeed <cedrickagapito@gmail.com>
2026-01-07 17:06:40 +0800

    updated footer copyright

--- src/components/Footers/FooterAdmin.js
+++ src/components/Footers/FooterAdmin.js
@@ -19,7 +19,9 @@ export default function FooterAdmin() {
                 >
                    
                 </a>
-                CYRILL JAMES B. FIDER Disclaimer: This project is purely for educational and presentation purposes only{" "}
+                CYRILL JAMES B. FIDER {" "}
+                <br/>
+                Disclaimer: This project is purely for educational and presentation purposes only
                 <a
                 href="https://https://www.slu-laboratory-resources.engineering/"
                 className="text-blueGray-500 hover:text-blueGray-700 text-sm font-semibold py-1"
